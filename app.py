f294bd002720d24fa288342e9979f76f01cf97a9
Zach <78088841@qq.com>
2019-04-26 16:17:51 +0800

    fix html dir

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 import sys
 import click
 import unittest
-# from flask import Flask, render_template, request, url_for, redirect, flash
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
-# from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, url_for, redirect, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from watchlist import app, db
 from watchlist.models import Movie, User
 from watchlist.commands import forge, initdb
