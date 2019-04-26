18ac9113f844028839335839a97ab9643293373d
Zach <78088841@qq.com>
2019-04-26 16:09:51 +0800

    add app

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import sys
 import click
-from flask import Flask, render_template, request, url_for, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
-
+import unittest
+# from flask import Flask, render_template, request, url_for, redirect, flash
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+# from werkzeug.security import generate_password_hash, check_password_hash
+from watchlist import app, db
+from watchlist.models import Movie, User
+from watchlist.commands import forge, initdb
 
 
 # SQLite URI compatible
